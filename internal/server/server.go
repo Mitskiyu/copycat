5afe6ad24ae482979d04ef8611fef42a986e031b
Denzel <denzel.mitski@gmail.com>
2025-02-27 12:21:49 +0100

    feat: add go file server

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,13 +4,9 @@ import (
 	"net/http"
 )
 
-
 func NewServer() *http.Server {
     mux := http.NewServeMux()
-
-    mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Hi, Copycat!"))
-    })
+    mux.Handle("/", Web())
 
     return &http.Server{
         Addr: ":8080",
